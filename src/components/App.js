d1142edd3316b56b175a5cd85763d7ab8df91efa
francescoMain <cesaranofrancescomain@gmail.com>
2024-01-26 15:55:49 +0100

    Add App component and AppBar component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ButtonAppBar from "./AppBar";
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar />
+    </div>
+  );
+}
+
+export default App;
